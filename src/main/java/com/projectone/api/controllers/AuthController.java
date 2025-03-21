51a1f782c19cbdb4792e4bc717ddbbf66634d171
Timofey Gavrishin <timofey.gavrishin@soprasteria.com>
2025-03-21 14:59:49 +0100

    Security with JWT implemented

--- src/main/java/com/projectone/api/controllers/AuthController.java
+++ src/main/java/com/projectone/api/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.projectone.api.controllers;
 
+import com.projectone.api.dto.LoginCredentials;
 import com.projectone.api.dto.User;
+import com.projectone.api.services.AuthService;
 import com.projectone.api.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +15,21 @@ import java.util.UUID;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthService authService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthService authService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UUID> saveUser(@RequestBody User user) {
+    public ResponseEntity<UUID> registerNewUser(@RequestBody User user) {
         UUID userId = userService.save(user);
         return new ResponseEntity<>(userId, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginCredentials loginCredentials) {
+        return new ResponseEntity<>(authService.login(loginCredentials), HttpStatus.OK);
     }
 }
