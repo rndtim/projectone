f33a829247254148ab894356ea5fecc9c76198cc
Timofey Gavrishin <timofey.gavrishin@soprasteria.com>
2025-04-03 09:04:40 +0200

    .env file added

--- src/main/java/com/projectone/Application.java
+++ src/main/java/com/projectone/Application.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+//        uncomment to use dotenv file
+//        Dotenv dotenv = Dotenv.load();
+//        dotenv.entries().forEach(entry -> {
+//            System.setProperty(entry.getKey(), entry.getValue());
+//        });
         SpringApplication.run(Application.class, args);
     }
 
