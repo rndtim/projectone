93594a8bc038a9e115965fe1bb1fb392b7ab61d7
Timofey Gavrishin <timofey.gavrishin@soprasteria.com>
2025-04-04 14:51:21 +0200

    Swagger added

--- src/main/java/com/projectone/api/controllers/AuthController.java
+++ src/main/java/com/projectone/api/controllers/AuthController.java
@@ -4,6 +4,8 @@ import com.projectone.api.dto.JWTResponse;
 import com.projectone.api.dto.LoginCredentials;
 import com.projectone.api.dto.User;
 import com.projectone.api.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,8 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Tag(name = "Authentication Controller", description = "Registration")
+    @Operation(summary = "Register new user")
     @PostMapping("/registration")
     public ResponseEntity<Boolean> registerNewUser(@RequestBody User user) {
         if (authService.register(user))
@@ -27,6 +31,8 @@ public class AuthController {
             return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
     }
 
+    @Tag(name = "Authentication Controller", description = "Login")
+    @Operation(summary = "Login user")
     @PostMapping("/login")
     public ResponseEntity<JWTResponse> login(@RequestBody LoginCredentials loginCredentials) {
         JWTResponse jwtResponse = authService.login(loginCredentials);
